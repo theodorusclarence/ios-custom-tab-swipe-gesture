a535475472670e3c1cc51d37eea91cffa3228285
Theodorus Clarence <theodorusclarence@gmail.com>
2022-05-15 15:54:49 +0700

    Initial Commit

---
+++ furniture-app/furniture_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  furniture_appApp.swift
+//  furniture-app
+//
+//  Created by Clarence on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct furniture_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
